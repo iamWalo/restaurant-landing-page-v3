9e9e0e7b607c9b522521607a0e7be0c203036a3b
iamWalo <136198398+iamWalo@users.noreply.github.com>
2024-08-19 20:06:45 +0100

    Add files via upload

---
+++ javaScript/script.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', () => {
+    document.querySelector('.loader').style.display = 'none';
+    document.querySelector('.content').style.display = 'block';
+});
+
+// vedios animations
+const videos = document.querySelectorAll('.myVideo');
+videos.forEach(video => {
+
+    video.currentTime = 1.58;
+    video.addEventListener('mouseover', () => {
+        video.currentTime = 0;
+        video.play();
+    });
+
+    video.addEventListener('mouseout', () => {
+        video.currentTime = 1.58;
+        video.pause();
+    });
+});
+// nav bar  animation
+const buttons = document.querySelectorAll('.sidebar nav ul li a');
+console.log(buttons);
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        buttons.classList.remove('clicked');
+        button.classList.add('clicked');
+    })
+})
